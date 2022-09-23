79c4dddf48f433dc45426a5d76a71be21996ce0f
mikedobrin <md@jelurida.com>
2022-09-23 18:06:06 +0200

    client server communication

--- server.js
+++ server.js
@@ -37,6 +37,15 @@ JSON.parse(fs.readFileSync(DATA_FILE))
         resultsByDate[dateOfMonth].push(workResult);
     })
 
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    next();
+});
+
 app.get('/health', (req, res) => {
   res.send('ok');
 });
