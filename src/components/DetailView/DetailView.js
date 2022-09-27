99415fcf7b456f7b11f360fc249e2cef907ddc80
mikedobrin <md@jelurida.com>
2022-09-27 18:08:53 +0200

    answers detail view

--- src/components/DetailView/DetailView.js
+++ src/components/DetailView/DetailView.js
@@ -1,4 +1,6 @@
 import { Outlet } from "react-router-dom";
+import "./DetailView.css";
+
 const DetailView = () => {
     return (<div className="detail-view">
         <Outlet />
--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getExerciseDetail } from '../../utils';
+import "./Exercise.css"
 
 const Exercise = ({reportDate}) => {
 
@@ -8,14 +9,39 @@ const Exercise = ({reportDate}) => {
     const [exerciseDetails, setExerciseDetails] = useState();
 
     useEffect(() => {
-        getExerciseDetail(id, reportDate).then(serverResponse => setExerciseDetails(serverResponse.result));
+        if (id !== undefined && reportDate !== undefined) {
+            getExerciseDetail(id, reportDate).then(serverResponse => setExerciseDetails(serverResponse.result));
+        }
     }, [id, reportDate]);
 
     return (
         <div className="exercise-detail">
-            <div>{id}</div>
-            <div>{reportDate}</div>
-            {exerciseDetails && JSON.stringify(exerciseDetails)}
+            <div className="exercise-detail-meta">
+                <div>{reportDate}</div>
+                <div>Opdracht: {id}</div>
+                {exerciseDetails && exerciseDetails.length > 0 && exerciseDetails[0].difficulty &&
+                    <div>Difficulty: {exerciseDetails[0].difficulty}</div>
+                }
+            </div>
+            {exerciseDetails && exerciseDetails.length > 0 &&
+                <>
+                    <div className="exercise-detail-answers exercise-detail-answers-header">
+                        <div>&#128337;</div>
+                        <div>&#128100;</div>
+                        <div>Antwoord ID</div>
+                        <div>&#x2611;</div>
+                    </div>
+                    {exerciseDetails.map((detail) => {
+                        const date = new Date(detail.submitDateTime);
+                        return <div className="exercise-detail-answers" key={detail.submittedAnswerId}>
+                            <div>{String(date.getUTCHours()).padStart(2, 0)}:{String(date.getUTCMinutes()).padStart(2, 0)}:{String(date.getUTCSeconds()).padStart(2, 0)}</div>
+                            <div>{detail.userId}</div>
+                            <div>{detail.submittedAnswerId}</div>
+                            <div>{detail.correct ? "Correct" : "Incorrect"}</div>
+                        </div>
+                    })}
+                </>
+            }
         </div>
     )
 };
